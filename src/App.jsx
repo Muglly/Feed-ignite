a7b1e533bb625c8646354acc4929ad3ae2f850c8
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2023-02-04 15:45:16 -0300

    Create Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import { Header } from "./components/header/header";
+import { Sidebar } from "./components/Sidebar/Sidebar";
 
 import "./styles/global.css";
 
+import styles from "./App.module.css";
+
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>post</main>
+      </div>
+    </>
+  );
 }
 
 export default App;
