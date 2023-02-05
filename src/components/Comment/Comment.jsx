2bdb8dae65c6df21da531eb6082f423943293041
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2023-02-05 18:02:27 -0300

    Comment component created

---
+++ src/components/Comment/Comment.jsx
@@ -0,0 +1,39 @@
+import { Trash, HandsClapping } from "phosphor-react";
+
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/Muglly.png" alt="foto de perfil" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Rafael Souza</strong>
+              <time
+                title="02 de Fevereiro de 2023"
+                dateTime="2023-02-05 10:40:02"
+              >
+                Há 1h atrás
+              </time>
+            </div>
+
+            <button title="deletar comentário">
+              <Trash />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <HandsClapping /> Aplaudir <span>05</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
