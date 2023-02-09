6a3ab432cd0fa0becaef2829f2b6332b06e4883b
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2023-02-09 07:38:17 -0300

    img logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import styles from "./Header.module.css";
 
-import IgniteLogo from "../../assets/ignite-logo.svg";
+import IgniteLogo from "../assets/ignite-logo.svg";
 
 export function Header() {
   return (
